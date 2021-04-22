b5b5a72076897c5e1d351a09a9eec3adf457faa4
Guilherme Marques <guilherme.marques@voxtecnologia.com.br>
2021-04-22 18:27:40 -0300

    Injecao de dependencia

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './../course.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 
@@ -10,31 +11,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
-  constructor() { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'Dezember, 2, 2019'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTPS',
-        imageUrl: '/assets/images/http.png',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'November, 2, 2019'
-      }
-    ]
+    this.courses = this.courseService.retrieveAll();
   }
 
 }
